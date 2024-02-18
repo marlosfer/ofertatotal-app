5529a50982a98866212898c073116f0d01442e7d
Marlos <marlos_gpi@live.com>
2024-02-18 06:22:08 -0300

    att icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="icon" type="image/x-icon" href="loja.ico">
+    
     <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
     <title>OFERTA TOTAL</title>
     
